ff13d0d0e631942c28bca2cf6b5534ca062d2b7e
Friederike Mierke <mierke@net.chalmers.se>
2023-01-16 16:00:56 +0100

    Add initial R folder content

---
+++ R/learning.R
@@ -0,0 +1,23 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos <- 10
+
+weight_kilos
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+2 + 2 #control + shift + P to get to options -> style active file
+
+2 + 2 # or ctrl + shift + A
+
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+
+
